b36b87d9d531add639696f8241caa7bff6b8cf59
Brody Dingel <brodydingel795@gmail.com>
2020-10-25 21:17:40 -0500

    Add initial dumb form

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,55 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from "react";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const styles = {
+	wrapper: {
+		backgroundColor: "blue",
+		display: "flex",
+		flexDirection: "column",
+		width: "200px",
+	},
+	formElement: {
+		display: "block",
+	},
+};
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const Home = () => {
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+	const handleEmailChange = (e) => {
+		setEmail(e.target.value);
+	};
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+	const handleMessageChange = (e) => {
+		setMessage(e.target.value);
+	};
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+	const handleSave = (e) => {
+		e.preventDefault();
+		console.log({ email, message });
+	};
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+	return (
+		<div style={styles.wrapper}>
+			<form onSubmit={handleSave}>
+				<input
+					type={"email"}
+					value={email}
+					placeholder={"Enter recipient email address"}
+					onChange={handleEmailChange}
+					style={styles.formElement}
+				/>
+				<input
+					type={"text"}
+					value={message}
+					placeholder={"Enter secret message"}
+					onChange={handleMessageChange}
+					style={styles.formElement}
+				/>
+				<input type={"submit"} value={"save"} />
+			</form>
+		</div>
+	);
+};
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default Home;
