89100dee9f57a9f8fcf483321362565a11f58423
Brody Dingel <brodydingel795@gmail.com>
2020-11-29 21:35:42 -0600

    WIP: using AnimatePresence to step through form steps

--- pages/send.js
+++ pages/send.js
@@ -1,23 +1,39 @@
+import React, {useState} from 'react';
+import {motion, AnimatePresence} from 'framer-motion';
+
 import Page from '../components/page';
 import Header from '../components/header';
-import Link from 'next/link';
 
 const Send = () => {
+    const [count, setCount] = useState(0);
+
     return (
         <Page title={'Send'}>
             <Header />
-            <p className={'mt-32'}>Send here</p>
-            {/* <Link href={'/'}> */}
-                <a
-                    className={'p-3 uppercase w-full md:w-auto md:inline-block text-center'}
-                    href={'/'}
+            <button
+                className={'mt-32'}
+                onClick={() => setCount(count + 1)}
+            >
+                {'up'}
+            </button>
+            <button
+                onClick={() => setCount(count - 1)}
+            >
+                {'down'}
+            </button>
+            <AnimatePresence>
+                <motion.div
+                    key={count}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
                 >
-                    {'Home'}
-                </a>
-    {/* </Link> */}
-
+                    {count % 2 === 0 ? <p>Even!</p> : <p>Odd!</p>}
+                    
+                </motion.div>
+            </AnimatePresence>
         </Page>
-    )
-}
+    );
+};
 
 export default Send;
