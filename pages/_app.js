3d882de9262cbaae451f822696248eb0c7ac737e
Brody Dingel <brodydingel795@gmail.com>
2020-12-04 22:10:43 -0600

    Make into PWA

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import Head from 'next/head'
 import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-};
+const MyApp = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                  name="viewport"
+                  content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+                <title>Secret Message App</title>
+
+                <link rel="manifest" href="/manifest.json" />
+            </Head>
+            <Component {...pageProps} />
+        </> 
+)};
 
 export default MyApp
