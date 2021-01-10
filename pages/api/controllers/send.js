c3979312340a6bac19b7e30274d031fd8e278f1e
Brody Dingel <brodydingel795@gmail.com>
2021-01-10 13:14:00 -0600

    Get rid of unnecessary validation

--- pages/api/controllers/send.js
+++ pages/api/controllers/send.js
@@ -1,5 +1,3 @@
-import {validate as uuidValidate} from 'uuid';
-
 import postMessageService from '../services/post-message-service';
 import sendEmailService from '../services/send-email-service';
 
@@ -17,18 +15,13 @@ export default async (req, res) => {
             isSelfDestructChecked
         });
 
-        if (uuidValidate(guid)) {
-            const link = `http://localhost:3000/view?id=${guid}&key=${key}&iv=${iv}&hmacKey=${hmacKey}`;
+        const link = `http://localhost:3000/view?id=${guid}&key=${key}&iv=${iv}&hmacKey=${hmacKey}`;
 
-            await sendEmailService(email, link, isSelfDestructChecked);
+        await sendEmailService(email, link, isSelfDestructChecked);
 
-            res.status(200).json({message: 'Success'});
-        } else {
-            res.status(500).json({
-                message: 'Something went wrong'
-            });
-        }
+        res.status(200).end();
     } catch (error) {
-        res.status(500).json({error});
+        console.log('error', error);
+        res.status(500).end();
     }
 };
