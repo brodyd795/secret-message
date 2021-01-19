1845a938d8109d1b0fd2a60da166e2d30cb0dda1
Brody Dingel <brodydingel795@gmail.com>
2021-01-18 22:43:46 -0600

    Fix more styles on mobile

--- components/forms/form-text.js
+++ components/forms/form-text.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const FormHeader = ({children}) =>
     <span
-        className={'block text-center text-2xl sm:text-3xl my-4'}
+        className={'block text-center text-2xl sm:text-3xl my-8'}
     >
         {children}
     </span>;
@@ -10,6 +10,13 @@ export const FormHeader = ({children}) =>
 export const FormSubHeader = ({children}) =>
     <span
         className={'block text-md text-center mt-2'}
+    >
+        {children}
+    </span>;
+
+export const MessageSubHeader = ({children, isWarning}) =>
+    <span
+        className={`block text-md my-2 mx-4 ${isWarning ? 'text-red-400' : null}`}
     >
         {children}
     </span>;
--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,17 @@ const Home = () =>
     <Page title={'Home | Secret Message'}>
         <Header />
         <div className={'flex flex-1 md:flex-none justify-center flex-col ml-4 sm:ml-16 md:items-center md:justify-end md:ml-0 md:mt-32'} style={{height: '30vh'}}>
-            <span className={'text-4xl'}>Send a secret message.</span>
-            <span className={'text-2xl mt-5 ml-3'}>Fully secure.</span>
-            <span className={'text-2xl ml-3'}>Dead simple.</span>
+            <span className={'text-4xl'}>{'Send a secret message.'}</span>
+            <span className={'text-2xl mt-5 ml-3'}>{'Fully secure.'}</span>
+            <span className={'text-2xl ml-3'}>{'Dead simple.'}</span>
         </div>
         <div className={'p-3 w-full flex justify-center'}>
             <Link href="/send">
                 <button
                     type={'button'}
-                    className={'w-full border h-16 mb-12 rounded md:w-auto md:mt-10 md:px-8 table-cell align-middle'}
+                    className={'w-full border outline-none focus:outline-none h-16 mb-12 rounded md:w-auto md:mt-10 md:px-8 table-cell align-middle'}
                 >
-                    <a>
-                        {'Send message'}
-                    </a>
+                    {'Send message'}
                 </button>
             </Link>
         </div>
