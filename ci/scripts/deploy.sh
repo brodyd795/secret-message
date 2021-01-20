7b51c2e8266822a8b5225f035756d6f4c370b19f
Brody Dingel <brodydingel795@gmail.com>
2021-01-19 20:17:10 -0600

    Test deploy

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-curl \
-    --data "env=${ENVIRONMENT}" \
-    --data "password=${DEPLOY_WEBHOOK_PASSWORD}" \
-    --data "project=${PROJECT}" \
-    https://dingel.dev/deploy
+echo "Node Version: $(node --version)"
+# curl \
+#     --data "env=${ENVIRONMENT}" \
+#     --data "password=${DEPLOY_WEBHOOK_PASSWORD}" \
+#     --data "project=${PROJECT}" \
+#     https://dingel.dev/deploy
