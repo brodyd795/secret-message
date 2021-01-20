81a22ba64a46a0a6b52c654db979ae3501c570e4
Brody Dingel <brodydingel795@gmail.com>
2021-01-19 20:20:41 -0600

    Bring back deploy script

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 echo "Node Version: $(node --version)"
-# curl \
-#     --data "env=${ENVIRONMENT}" \
-#     --data "password=${DEPLOY_WEBHOOK_PASSWORD}" \
-#     --data "project=${PROJECT}" \
-#     https://dingel.dev/deploy
+curl \
+    --data "env=${ENVIRONMENT}" \
+    --data "password=${DEPLOY_WEBHOOK_PASSWORD}" \
+    --data "project=${PROJECT}" \
+    https://dingel.dev/deploy
