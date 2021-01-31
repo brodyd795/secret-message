5fbce5802071a3f5b70714317c2786a720e6b00c
Brody Dingel <brodydingel795@gmail.com>
2021-01-31 09:57:34 -0600

    Add about page

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 import Copyright from './copyright';
 
 const Footer = () => (
-    <div className={'text-center'}>
-        {/* <Copyright /> */}
+    <div className={'text-center mt-10'}>
+        <Copyright />
     </div>
 );
 
