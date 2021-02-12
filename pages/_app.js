715368d1da85f17f8f0fb770419e5b4c4aec8ae7
Brody Dingel <brodydingel795@gmail.com>
2021-02-12 13:50:20 -0600

    Fix checkbox styles after merge

--- pages/_app.js
+++ pages/_app.js
@@ -47,37 +47,61 @@ export default ({Component, pageProps}) => {
                 global
             >
                 {`
-                html {
-                    box-sizing: border-box;
-                }
-                
-                body {
-                    color: #adafbb;
-                    font-size: 16px;
-                    background-color: #03060c;
-                    font-family: 'JetBrains Mono', monospace;
-                }
-                
-                html,
-                body {
-                    width: 100%;
-                    margin: 0;
-                }
-                
-                a {
-                    color: #c6c6c6;
-                    cursor: pointer;
-                }
-                
-                p {
-                    margin: 0 0 8px;
-                }
-                
-                #__next {
-                    display: flex;
-                    flex-direction: column;
-                    min-height: 100vh;
-                }`}
+                    html {
+                        box-sizing: border-box;
+                    }
+                    
+                    body {
+                        color: #adafbb;
+                        font-size: 16px;
+                        background-color: #03060c;
+                        font-family: 'JetBrains Mono', monospace;
+                    }
+                    
+                    html,
+                    body {
+                        width: 100%;
+                        margin: 0;
+                    }
+                    
+                    a {
+                        color: #c6c6c6;
+                        cursor: pointer;
+                    }
+                    
+                    p {
+                        margin: 0 0 8px;
+                    }
+                    
+                    #__next {
+                        display: flex;
+                        flex-direction: column;
+                        min-height: 100vh;
+                    }
+                    
+                    .checkbox:before {
+                        content: '';
+                        position: absolute;
+                        width: 1.25rem;
+                        height: 1.25rem;
+                        border-radius: 50%;
+                        top: 0;
+                        left: 0;
+                        transform: scale(1.1);
+                        box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.2);
+                        background-color: white;
+                        transition: 0.2s ease-in-out;
+                    }
+            
+                    .checkbox:checked {
+                        @apply: bg-indigo-400;
+                        background-color: #7f9cf5;
+                    }
+            
+                    .checkbox:checked:before {
+                        left: 1.25rem;
+                    }
+                `}
             </style>
         </>
     );
